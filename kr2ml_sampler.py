9f57df53210d71f9786f055075c8828ed74c070e
pawan.mandrake <pawan.kumar@cse.iitd.ac.in>
2020-02-27 15:13:47 +0530

    serializer for tab separated file into a single file, with paraphrases listed one after other.

---
+++ kr2ml_sampler.py
@@ -0,0 +1,12 @@
+filename='kr2ml_select.tsv'
+with open(filename, 'r') as f_read:
+    lines =f_read.readlines()
+
+filename='nlqs_select.txt'
+list_nlqs_with_pp = []
+for line in lines:
+    [list_nlqs_with_pp.append(nlq) for nlq in line.split('\t')[1:3]]
+
+with open(filename, 'w') as f_write:
+    f_write.writelines('\n'.join(list_nlqs_with_pp))
+
