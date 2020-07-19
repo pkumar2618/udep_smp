3c64e605bbc2a53af54c6ff4bf9f44417f71e965
pawan.mandrake <pawan.kumar@cse.iitd.ac.in>
2020-07-20 04:23:04 +0530

    starting new branch

--- run_udep_lambda.sh
+++ run_udep_lambda.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-cp udepl_nlq.txt /Users/pawan/projects/UDepLambda/udepl_nlq.txt
-cd /Users/pawan/projects/UDepLambda/
-cat /Users/pawan/projects/UDepLambda/udepl_nlq.txt | /Users/pawan/projects/UDepLambda/run-english.sh
+cp udepl_nlq.txt /home/pawan/projects/UDepLambda/udepl_nlq.txt
+cd /home/pawan/projects/UDepLambda/ 
+cat /home/pawan/projects/UDepLambda/udepl_nlq.txt | /home/pawan/projects/UDepLambda/run-english.sh
